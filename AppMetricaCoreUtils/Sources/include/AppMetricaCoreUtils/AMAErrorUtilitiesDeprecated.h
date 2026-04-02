20ce7229b43dee958a2392cd906fc7ca1d7626e6
alex-oleynik <alex-oleynik@yandex-team.com>
2026-04-02 18:41:21 +0300

    restored const for compatibility commit_hash:97ef2988600e0b50c9a70780b3e5f9d09b6e8f7f

--- AppMetricaCoreUtils/Sources/include/AppMetricaCoreUtils/AMAErrorUtilitiesDeprecated.h
+++ AppMetricaCoreUtils/Sources/include/AppMetricaCoreUtils/AMAErrorUtilitiesDeprecated.h
@@ -1,9 +1,17 @@
 
 #import <Foundation/Foundation.h>
 
+#if __has_include("AMAErrorUtilities.h")
+    #import "AMAErrorUtilities.h"
+#else
+    #import <AppMetricaCoreUtils/AMAErrorUtilities.h>
+#endif
+
 extern NSErrorDomain const kAMAAppMetricaErrorDomain __attribute__((deprecated("Use AMAAppMetricaErrorDomain instead")));
 
 extern NSErrorDomain const kAMAAppMetricaInternalErrorDomain __attribute__((deprecated("Use AMAAppMetricaInternalErrorDomain instead")));
 extern NSErrorDomain const kAMAAppMetricaDatabaseErrorDomain __attribute__((deprecated("Use AMAAppMetricaDatabaseErrorDomain instead")));
 
-#define AMAAppMetricaInternalEventJsonSerializationError (AMAAppMetricaInternalEventErrorCodeJsonSerialization)
+static const NSUInteger AMAAppMetricaEventErrorCodeInitializationError __attribute__((deprecated("Use AMAAppMetricaEventErrorCodeIsNotActivated instead"))) = AMAAppMetricaEventErrorCodeIsNotActivated;
+
+static const NSUInteger AMAAppMetricaInternalEventJsonSerializationError __attribute__((deprecated("Use AMAAppMetricaInternalEventErrorCodeJsonSerialization instead"))) = AMAAppMetricaInternalEventErrorCodeJsonSerialization;
